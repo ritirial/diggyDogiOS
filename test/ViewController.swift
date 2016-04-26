15f5a03cb6f40110a40eaf464deb5b8d2bb663e6
Alumno <alumno@lab1-173.pue.itesm.mx>
2016-04-25 19:19:50 -0500

    second commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("this is a test")
+        print("this is a test 2")
     }
 
     override func didReceiveMemoryWarning() {
